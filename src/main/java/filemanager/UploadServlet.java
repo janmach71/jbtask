c0a1da2d02dd7d1f64611bdff8d1e7c156a7a0c8
Jan Mach <jan.nec.mach@gmail.com>
2015-05-05 21:19:02 +0200

    test api.jsp

--- src/main/java/filemanager/UploadServlet.java
+++ src/main/java/filemanager/UploadServlet.java
@@ -50,7 +50,7 @@ public class UploadServlet extends HttpServlet {
         }
 
         request.setAttribute("message", "Upload has been done successfully into " + savePath + " !");
-        getServletContext().getRequestDispatcher("/message.jsp").forward(
+        getServletContext().getRequestDispatcher("/api/v1/message.jsp").forward(
                 request, response);
     }
 
