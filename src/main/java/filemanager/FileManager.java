c9e7c1564ae1c078f85d3dab00a77c40e03eebe4
Jan Mach <jan.mach71@gmail.com>
2015-05-05 19:27:44 +0200

    minimize js

--- src/main/java/filemanager/FileManager.java
+++ src/main/java/filemanager/FileManager.java
@@ -22,6 +22,32 @@ public class FileManager {
         }
         return list;
     }
+    public static List<DirItem> getDirInZip(String zip,String dir) {
+        File zipfile = new File(zip);
+        try {
+            ZipInputStream zip=new ZipInputStream(new FileInputStream(file));
+            ZipEntry entry=zip.getNextEntry();
+            while (entry != null) {
+                zip.closeEntry();
+                entry=zip.getNextEntry();
+            }
+        }
+        catch (  FileNotFoundException e) {
+            throw new MojoFailureException(this,"Cant open ANE file " + file.getPath(),e.getLocalizedMessage());
+        }
+        catch (  IOException e) {
+            throw new MojoFailureException(this,"Cant read ANE file " + file.getPath(),e.getLocalizedMessage());
+        }
+        File[] listOfFiles = folder.listFiles();
+
+        List<DirItem> list = new ArrayList<DirItem>(listOfFiles.length);
+
+        for (File file : listOfFiles) {
+            DirItem item = new DirItem(file);
+            list.add(item);
+        }
+        return list;
+    }
     public static void getDirJson(Writer out, String dir) throws IOException{
         //keep in list, so we can sort on arbitrary property
         List<DirItem> list = getDir(dir);
