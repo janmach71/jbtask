deff0f237042279d635f9bc96c5954ad7dd1c024
Jan Mach <jan.nec.mach@gmail.com>
2015-05-05 21:16:38 +0200

    test api.jsp

--- src/main/java/filemanager/UploadServlet.java
+++ src/main/java/filemanager/UploadServlet.java
@@ -16,7 +16,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.Part;
 
 
-@WebServlet("/UploadServlet")
+@WebServlet("/api/v1/UploadServlet")
 @MultipartConfig(fileSizeThreshold=1024*1024*2, // 2MB
         maxFileSize=1024*1024*10,      // 10MB
         maxRequestSize=1024*1024*50)   // 50MB
