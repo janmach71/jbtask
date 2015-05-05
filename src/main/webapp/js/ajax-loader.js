4c770b59eaf837299cb66fd2ff8a4cc04bc093f5
Jan Mach <jan.mach71@gmail.com>
2015-05-05 10:51:30 +0200

    change to FIFO for opening stored folders - fix

--- src/main/webapp/js/ajax-loader.js
+++ src/main/webapp/js/ajax-loader.js
@@ -2,13 +2,13 @@ $(document).ready(function () {
     $(document).ajaxStart(function () {
         //console.log("add");
         $(".ajax-loader").fadeIn("slow")
-        $(".ajax-loader-background").fadeIn("slow")
-        $(".ajax-loader-foreground").fadeIn("slow")
+        $(".ajax-loader-background").fadeIn("fast")
+        $(".ajax-loader-foreground").fadeIn("fast")
     });
     $(document).ajaxStop(function () {
         //console.log("remove");
         $(".ajax-loader").fadeOut("slow")
-        $(".ajax-loader-background").fadeOut("slow")
-        $(".ajax-loader-foreground").fadeOut("slow")
+        $(".ajax-loader-background").fadeOut("100")
+        $(".ajax-loader-foreground").fadeOut("100")
     });
 });
