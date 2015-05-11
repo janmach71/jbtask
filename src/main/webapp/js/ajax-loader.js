a6e98ee2758313c10fed0171775f58357de2dc09
Jan Mach <jan.nec.mach@gmail.com>
2015-05-12 00:12:01 +0200

    adjust loader indicator fade in / out

--- src/main/webapp/js/ajax-loader.js
+++ src/main/webapp/js/ajax-loader.js
@@ -2,13 +2,13 @@ $(document).ready(function () {
     $(document).ajaxStart(function () {
         //console.log("add");
         $(".ajax-loader").fadeIn("slow")
-        $(".ajax-loader-background").fadeIn("fast")
-        $(".ajax-loader-foreground").fadeIn("fast")
+        $(".ajax-loader-background").fadeIn("0")
+        $(".ajax-loader-foreground").fadeIn("0")
     });
     $(document).ajaxStop(function () {
         //console.log("remove");
         $(".ajax-loader").fadeOut("slow")
-        $(".ajax-loader-background").fadeOut("100")
-        $(".ajax-loader-foreground").fadeOut("100")
+        $(".ajax-loader-background").fadeOut("50")
+        $(".ajax-loader-foreground").fadeOut("50")
     });
 });
